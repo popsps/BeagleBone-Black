45de90ceb91012c89611cce3faf2c94899080673
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2022-09-21 16:47:34 -0400

    Create 05_read-button-GPIO.py
    init

---
+++ python-demo/05_read-button-GPIO.py
@@ -0,0 +1,7 @@
+# Digital reads allow pins to read true or false, or high or low (binary)
+# When doing digital reads from the pins that we have setup as inputs(P9_13, P9_15 in this case), 
+# the BeagleBone black wants to operate at 3.3 volts and 
+# so we need to make sure that we're refrencing things to 3.3 volts and not 5 volts and so we're very careful that
+# our rail is established by pin P9_3 not P9_5
+
+# p9_1 is ground. So we're making a 3.3 volt rail. use 1000ohm pull down resistor or above. but not belo.
