31e3fa6bd769b816ecfefb058c3e14e46b1312c9
Shayan Hosseini <59066663+popsps@users.noreply.github.com>
2022-09-21 19:55:13 -0400

    Update 05_read-button-GPIO.py

--- python-demo/05_read-button-GPIO.py
+++ python-demo/05_read-button-GPIO.py
@@ -5,3 +5,26 @@
 # our rail is established by pin P9_3 not P9_5
 
 # p9_1 is ground. So we're making a 3.3 volt rail. use 1000ohm pull down resistor or above. but not belo.
+
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+topButton = "P9_11"
+bottomButton = "P9_13"
+
+GPIO.setup(topButton, GPIO.IN)
+GPIO.setup(bottomButton, GPIO.IN)
+
+while(1):
+  if(GPIO.input(topButton)):
+    print("Top Button is pressed")
+   if(GPIO.input(bottomButton)):
+    print("Bottom Button is pressed")
+   if GPIO.input(topButton) and GPIO.input(bottomButton):
+    break;
+   sleep(.2)
+
+print("Goodby, Come again soon")
+GPIO.cleanup()
+    
+    
