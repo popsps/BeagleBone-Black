f2d0afc15a24ddcd526f1ad5b43e12ff037c0ead
vb <vb@box.com>
2022-09-23 08:24:18 -0400

    add stub

---
+++ c-demo/gpio-stub.c
@@ -0,0 +1,19 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "gpio.h"
+
+int gpio_export(unsigned int gpio) {
+  printf("Running GPIO Stub\n");
+  return 0;
+}
+
+int gpio_unexport(unsigned int gpio) { return 0; }
+
+int gpio_set_direction(unsigned int gpio, PIN_DIRECTION out_flag) { return 0; }
+int gpio_set_value(unsigned int gpio, PIN_VALUE value) { return 0; }
