139d3120070e66212996253ad87e8d72481a2dcf
popsps <popsps17@gmail.com>
2022-09-24 01:16:33 +0000

    update

--- c-demo/hw2.c
+++ c-demo/hw2.c
@@ -9,7 +9,11 @@
 #include "signal.h"
 
 /**
- * Using GPIO 9_23 and GPIO 9_24 GPIO 2_25
+ * Each side gets green light for 2 minutes. The transition to the red light
+ * should have a 5 second yellow signal.
+ *
+ * Using GPIO 9_11 and GPIO 9_12 GPIO 2_13 for signal 1
+ * Using GPIO 9_23 and GPIO 9_24 GPIO 2_26 for signal 2
  **/
 typedef struct trafic_signal_struct {
   int red_light;
@@ -19,6 +23,7 @@ typedef struct trafic_signal_struct {
 
 void cleanUp(trafic_signal* signal);
 void initilize(trafic_signal* signal);
+void unset_signal(trafic_signal* signal);
 void makeSignalRed(trafic_signal* signal);
 void makeSignalYellow(trafic_signal* signal);
 void makeSignalGreen(trafic_signal* signal);
@@ -47,30 +52,26 @@ int main(int argc, char* argv[]) {
   cleanUp(&signal2);
   initilize(&signal1);
   initilize(&signal2);
+  unset_signal(&signal1);
+  unset_signal(&signal2);
 
-  sleep(2);
-  for (size_t i = 0; i < 5; i++) {
+  // start the signals for intersection
+  sleep(1);
+  do {
     makeSignalGreen(&signal1);
     makeSignalRed(&signal2);
-    sleep(4);
+    sleep(10);
     makeSignalYellow(&signal1);
     makeSignalYellow(&signal2);
-    sleep(2);
+    sleep(2.5);
     makeSignalRed(&signal1);
     makeSignalGreen(&signal2);
-    sleep(4);
-  }
-  // unset all GPIO pins
-  gpio_set_value(signal1.red_light, LOW);
-  gpio_set_value(signal1.yellow_light, LOW);
-  gpio_set_value(signal1.green_light, LOW);
+    sleep(10);
+    makeSignalYellow(&signal1);
+    makeSignalYellow(&signal2);
+    sleep(2.5);
+  } while (1);
 
-  gpio_set_value(signal2.red_light, LOW);
-  gpio_set_value(signal2.yellow_light, LOW);
-  gpio_set_value(signal2.green_light, LOW);
-
-  printf("Clean up Pins...\n");
-  sleep(4);
   // cleanUp(&signal1);
   // cleanUp(&signal2);
   printf("CS 695 HW2 Simple Intersection with Opposing Traffic Lights Done\n");
@@ -110,17 +111,23 @@ void makeSignalGreen(trafic_signal* signal) {
   gpio_set_value(signal->yellow_light, LOW);
   gpio_set_value(signal->green_light, HIGH);
 }
-
+void unset_signal(trafic_signal* signal) {
+  gpio_set_value(signal->red_light, LOW);
+  gpio_set_value(signal->yellow_light, LOW);
+  gpio_set_value(signal->green_light, LOW);
+}
 void sig_handler(int sig) {
   if (sig == SIGINT) {
-    // printf("SIGINT\n");
     shell_write("Recived SIGINT");
     unset_signal(&signal1);
     unset_signal(&signal2);
     shell_write("Pins are cleaned up.");
     _exit(EXIT_SUCCESS);
   } else if (sig == SIGTSTP) {
-    printf("SIGTSTP\n");
+    shell_write("Recived SIGTSTP");
+    unset_signal(&signal1);
+    unset_signal(&signal2);
+    shell_write("Pins are cleaned up.");
     _exit(EXIT_SUCCESS);
   }
 }
---
+++ c-demo/readme.md
@@ -0,0 +1,5 @@
+# Running HW2
+
+```shell
+gcc -O2 -Wall pins.h signal.c gpio.c hw2.c -o hw2
+```
