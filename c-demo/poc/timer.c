c607c48b9a8e0440415cbb1b56b91fa4a52b4c61
Shayan Hosseini <popsps17@gmail.com>
2022-10-09 14:42:05 -0400

    timer

--- c-demo/poc/timer.c
+++ c-demo/poc/timer.c
@@ -7,18 +7,25 @@
 
 int main(int argc, char* argv[]) {
   time_t now;
-  struct tm* timeInfo;
+  struct tm *timeInfo, *utcTimeInfo;
   time(&now);
   printf("now: %ld\n", now);
   time_t base = time(0);
   printf("base: %ld\n", base);
 
   timeInfo = localtime(&now);
+  utcTimeInfo = gmtime(&now);
+  char buff[20];
+  printf("Current local time and date: %s\n", asctime(timeInfo));
+  printf("Event occurred at (UTC): %s\n", asctime(utcTimeInfo));
+  strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", utcTimeInfo);
+  printf("Event occurred at (UTC): %s\n", buff);
+
   // printf("Current local time and date: %s", now);
   // printf("Current local time and date: %s", timeInfo);
-  printf("Current local time and date: %s\n", asctime(timeInfo));
 
   time_t now2 = base;
+  
   while (now2 - base < 5) now2 = time(0);
   printf("now2: %ld\n", now2);
 
