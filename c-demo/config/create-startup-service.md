9c8b630667b92e78aa4cb9660ce7b1088459a595
wsl <wsl@wsl.com>
2022-11-20 17:45:19 -0500

    u s

---
+++ c-demo/config/create-startup-service.md
@@ -0,0 +1,9 @@
+```sh
+cd /usr/bin/
+nano gps.sh
+```
+
+```
+#!/bin/bash
+/home/root/name_of_compiled_code
+```
--- c-demo/poc/12-startup.c
+++ c-demo/poc/12-startup.c
@@ -4,6 +4,14 @@
 #include <time.h>
 #include <unistd.h>
 
+/**
+ *
+ * #!/bin/bash
+ * /home/debian/BeagleBone-Black/c-demo/poc/12-startup
+ * chmod u+x /usr/bin/gps-script.sh
+ *
+ *
+ */
 int main(int argc, char* argv[]) {
   time_t current_time;
   struct tm* utcTimeInfo;
@@ -12,5 +20,7 @@ int main(int argc, char* argv[]) {
   char timeInfoBuffer[25] = {0};
   strftime(timeInfoBuffer, sizeof(timeInfoBuffer), "%Y-%m-%d %H:%M:%S", utcTimeInfo);
   fprintf(stdout, "%s - Running this application on boot\n", timeInfoBuffer);
+  FILE* fp = fopen("./test.txt", "a+");
+  fprintf(fp, "%s - Running this application on boot\n", timeInfoBuffer);
   return 0;
 }
