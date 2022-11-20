644bc48989f059b1d3a8946e98d90698317af7be
wsl <wsl@wsl.com>
2022-11-19 23:26:55 -0500

    update poc

--- c-demo/poc/09-uart.c
+++ c-demo/poc/09-uart.c
@@ -5,16 +5,49 @@
 #include <termio.h>
 #include <time.h>
 #include <unistd.h>
-// sudo apt-get install minicom
-// sudo minicom -b 115200 -D /dev/tty4
-// echo "Using echo" >> /dev/tty4
+
+/**
+ * Enabling UART
+ *
+ * BB-UART1-00A0 => maps UART1 to P9.24 (Tx) and P9.26 (Rx)
+ * BB-UART2-00A0 => maps UART2 to P9.21 (Tx) and P9.22 (Rx)
+
+ * You need to edit /boot/uEnv.txt:
+
+ * Change these two lines:
+
+ * #uboot_overlay_addr6=/lib/firmware/<file6>.dtbo
+ * #uboot_overlay_addr7=/lib/firmware/<file7>.dtbo
+
+ * to
+
+ * uboot_overlay_addr6=/lib/firmware/BB-UART4-00A0.dtbo
+ * uboot_overlay_addr7=/lib/firmware/BB-UART2-00A0.dtbo
+
+ * and then reboot.
+ *
+ * The UARTs are connected to /dev/ttySn:
+ * UART0 (console header) /dev/ttyS0 -- note: /sbin/agetty is running on this port
+ * UART1 P9.24,P9.26 /dev/ttyS1
+ * UART2 P9.21,P9.22 /dev/ttyS2
+ * UART3 P9.42 (tx only) /dev/ttyS3
+ * UART4 P9.13,P9.11 /dev/ttyS4
+ * UART5 P8.37,P8.38 /dev/ttyS5
+ * ls -l ttyO*
+ * echo "Using echo" >> /dev/ttyO4
+ * sudo apt-get install minicom
+ * sudo minicom -b 115200 -D /dev/ttyO4
+ * sudo picocom -b 115200 /dev/ttyO4
+ * groups $USER if not dialout then sudo gpasswd --add $USER dialout
+ * sudo chmod 666 /dev/ttyO4
+*/
 int main(int argc, char* argv[]) {
   int file, count;
   if (argc != 2) {
     printf("Please pass a message string to send, exiting!\n");
     return -2;
   }
-  file = open("/dev/tty4", O_RDWR | O_NOCTTY | O_NDELAY);
+  file = open("/dev/ttyO4", O_RDWR | O_NOCTTY | O_NDELAY);
   if (file < 0) {
     perror("UART: Failed to open the device.\n");
     return -1;
@@ -26,6 +59,7 @@ int main(int argc, char* argv[]) {
   tcflush(file, TCIFLUSH);
   tcsetattr(file, TCSANOW, &options);
   // send the string plus the null character
+  printf("sending %s", argv[1]);
   count = write(file, argv[1], strlen(argv[1] + 1));
   if (count < 0) {
     perror("UART Failed to write to the output.\n");
