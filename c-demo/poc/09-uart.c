8e146d760f7eb793dc6afa72c5e1e0d163ad79c0
wsl <wsl@wsl.com>
2022-11-19 23:33:33 -0500

    g

--- c-demo/poc/09-uart.c
+++ c-demo/poc/09-uart.c
@@ -61,7 +61,7 @@ int main(int argc, char* argv[]) {
   tcsetattr(file, TCSANOW, &options);
   // send the string plus the null character
   printf("sending %s\n", argv[1]);
-  count = write(file, argv[1], strlen(argv[1] + 1));
+  // count = write(file, argv[1], strlen(argv[1] + 1));
   count = fprintf(fp, "%s", argv[1]);
   if (count < 0) {
     perror("UART Failed to write to the output.\n");
