f25af3e7d912c2ab40a38bfab281f7e0bf5cba1f
wsl <wsl@wsl.com>
2022-11-21 09:29:23 -0500

    update main

--- c-demo/project/main.c
+++ c-demo/project/main.c
@@ -168,13 +168,13 @@ void* handle_gps_sensor(void* ptr) {
 void* handle_logger(void* ptr) {
   b_log(DEBUG, "[THREAD%ld-ACTION]: Starting the LOGGER THREAD...", logger_thread);
   while (1) {
-    pthread_rwlock_rdlock(&isOn);
+    pthread_rwlock_rdlock(&isOn_rwlock);
     if (isOn) {
       pthread_rwlock_rdlock(&temp_rwlock);
       b_log(INFO, "[THREAD%ld-TEMPERATURE]: mv=%.2f C=%.2f F=%.2f", logger_thread, millivolts, temp_c, temp_f);
       pthread_rwlock_unlock(&temp_rwlock);
     }
-    pthread_rwlock_unlock(&isOn);
+    pthread_rwlock_unlock(&isOn_rwlock);
     sleep(1);
   }
   return NULL;
