77d431b0a7a937f7df056989ec4b341cc08b9383
wsl <wsl@wsl.com>
2022-11-22 14:50:18 -0500

    update poc

---
+++ python-demo/14_gps_test.py
@@ -0,0 +1,10 @@
+import serial
+import Adafruit_BBIO.UART as UART
+UART.setup("UART1")
+serial = serial.Serial('/dev/ttyO4', 9600)
+while (1):
+    print('start reading...')
+    while serial.inWaiting() == 0:
+        pass
+    NMEA = serial.readline()
+    print(NMEA)
