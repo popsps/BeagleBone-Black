81fcda853f45d6c16905968f89d7bf5e5c20c046
wsl <wsl@wsl.com>
2022-11-22 20:28:38 -0500

    update poc

--- c-demo/project/main.c
+++ c-demo/project/main.c
@@ -158,12 +158,12 @@ void* handle_gps_sensor(void* ptr) {
   uart_init(4);
 
   while (1) {
-    char* nmea = serial_read_line();
-    if (nmea != NULL && (nmea[0] != '\0' || nmea[0] != '\n')) {
-      b_log(INFO, "[THREAD%ld-NMEA]: %s: ", gps_thread, nmea);
-    }
+    // char* nmea = serial_read_line();
+    // if (nmea != NULL && (nmea[0] != '\0' || nmea[0] != '\n')) {
+    //   b_log(INFO, "[THREAD%ld-NMEA]: %s: ", gps_thread, nmea);
+    // }
 
-    sleep(1);
+    // sleep(1);
   }
   return NULL;
 }
