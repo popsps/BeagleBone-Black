cd6c49d97ef9313ab1d9d4f3b9775315cebba643
wsl <wsl@wsl.com>
2022-11-23 14:00:56 -0500

    update poc

--- c-demo/include/uart.c
+++ c-demo/include/uart.c
@@ -48,7 +48,7 @@ char* serial_read_line() {
   memset(buffer, 0, sizeof(char) * 1024);
   // printf("I'm here serial_read_line 1 %s\n", buffer);
   while (i < 1024 && count < 0) {
-    count = read(fd, (void*)(buffer + i), 1);
+    count = read(fd, (void*)(buffer + i), 1024);
     // printf("I'm here serial_read_line 2 %s %d\n", buffer, count);
     if (buffer[i] == '\n') {
       return buffer;
