4239fe25516d1a12f37df48065002feeeb62c4ce
wsl <wsl@wsl.com>
2022-11-22 21:47:45 -0500

    update poc

--- c-demo/project/main.c
+++ c-demo/project/main.c
@@ -169,6 +169,7 @@ void* handle_gps_sensor(void* ptr) {
 
   while (1) {
     char* nmea = serial_read_line();
+     b_log(INFO, "[THREAD%ld-NMEA]:", gps_thread);
     if (nmea != NULL && nmea[0] != '\0' && nmea[0] != '\n') {
       // b_log(INFO, "[THREAD%ld-NMEA]: %s", gps_thread, nmea);
       b_log(INFO, "[THREAD%ld-NMEA]: %s", gps_thread, nmea);
