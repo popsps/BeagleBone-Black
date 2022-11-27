194cce4ce83c534db698cd0fd484513e1903519d
wsl <wsl@wsl.com>
2022-11-27 08:26:44 -0500

    update poc

--- c-demo/poc/09-uart.c
+++ c-demo/poc/09-uart.c
@@ -41,6 +41,28 @@
  * groups $USER if not dialout then sudo gpasswd --add $USER dialout
  * sudo chmod 666 /dev/ttyO4
 */
+
+#define UPDATE_10_sec "$PMTK220,10000*2F\r\n"  // Update Every 10 Seconds
+#define UPDATE_5_sec "$PMTK220,5000*1B\r\n"    // Update Every 5 Seconds
+#define UPDATE_1_sec "$PMTK220,1000*1F\r\n"    // Update Every One Second
+#define UPDATE_200_msec "$PMTK220,200*2C\r\n"  // Update Every 200 Milliseconds
+// This set is used to set the rate the GPS takes measurements
+#define MEAS_10_sec "$PMTK300,10000,0,0,0,0*2C\r\n"  // Measure every 10 seconds
+#define MEAS_5_sec "$PMTK300,5000,0,0,0,0*18\r\n"    // Measure every 5 seconds
+#define MEAS_1_sec "$PMTK300,1000,0,0,0,0*1C\r\n"    // Measure once a second
+#define MEAS_200_msec "$PMTK300,200,0,0,0,0*2F\r\n"  // Measure 5 times a second
+// Set the Baud Rate of GPS
+#define BAUD_57600 "$PMTK251,57600*2C\r\n"  // Set Baud Rate at 57600
+// default
+#define BAUD_9600 "$PMTK251,9600*17\r\n"  // Set 9600 Baud Rate
+// Commands for which NMEA Sentences are sent
+// Send only the GPRMC Sentence
+#define GPRMC_ONLY "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"
+// Send GPRMC AND GPGGA Sentences
+#define GPRMC_GPGGA "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"
+#define SEND_ALL "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"      // Send All Sentences
+#define SEND_NOTHING "$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"  // Send Nothing
+
 int main(int argc, char* argv[]) {
   int file, count;
   if (argc != 2) {
@@ -98,6 +120,7 @@ int main(int argc, char* argv[]) {
   int i = 0;
 
   memset(buffer, 0, sizeof(buffer));
+
   while (1) {
     count = read(file, buffer + i, 1);
     if (count <= 0) {
@@ -115,7 +138,7 @@ int main(int argc, char* argv[]) {
           printf("%s - %s\n", timeInfoBuffer, buffer);
           // printf("%s", buffer);
         }
-        tcflush(file, TCIOFLUSH);
+        // tcflush(file, TCIOFLUSH);
         memset(buffer, 0, sizeof(buffer));
         i = 0;
       } else {
